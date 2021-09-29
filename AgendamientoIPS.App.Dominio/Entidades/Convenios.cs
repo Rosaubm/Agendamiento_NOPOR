3fa65f99197b3954934fe98bf706008fcb28b3b3
Neyla Villamizar <90814857+NeylaVillamizar@users.noreply.github.com>
2021-09-28 19:02:46 -0500

    Update Convenios.cs

--- AgendamientoIPS.App.Dominio/Entidades/Convenios.cs
+++ AgendamientoIPS.App.Dominio/Entidades/Convenios.cs
@@ -4,8 +4,8 @@ namespace AgendamientoIPS.App.Dominio
 {
     public class Convenios
     {
-        public String NumeroConvenio {get;set;} // Cita Presencial si o no 
-        public String NombreEPS {get;set;} // Cita Virtual si o no S
-        public Int Descuento {get;set;} // Horario de CitaS
+        public String NumeroConvenio {get;set;} 
+        public String NombreEPS {get;set;} 
+        public Int Descuento {get;set;}
     }
 } 
