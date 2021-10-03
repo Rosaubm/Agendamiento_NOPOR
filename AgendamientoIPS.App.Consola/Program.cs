c78f20daf9cec933c240db526641f71b453774f5
P4t0 <gialemon@gmail.com>
2021-10-03 12:55:50 -0500

    Repositorios Paciente y Pruebas Add y Buscar Paciente

--- AgendamientoIPS.App.Consola/Program.cs
+++ AgendamientoIPS.App.Consola/Program.cs
@@ -1,13 +1,40 @@
 ﻿using System;
 using AgendamientoIPS.App.Dominio;
+using AgendamientoIPS.App.Persistencia;
 
 namespace AgendamientoIPS.App.Consola
 {
     class Program
     {
+        private static IRepositorioPaciente _repoPaciente = new RepositorioPaciente(new Persistencia.AppContext());
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            //AddPaciente();
+            BuscarPaciente(1);
+        }
+
+        private static void AddPaciente()
+        {
+            var paciente = new Paciente
+            {
+                Nombre = "Celeste",
+                PrimerApellido = "Best",
+                SegundoApellido = "Burks",
+                Direccion = "4466 Laoreet Rd.",
+                Telefono = "(238) 782-3612",
+                Correo = "sed.eu@risus.org",
+                EPS = EPS.CoomevaEPS
+            };
+            _repoPaciente.AddPaciente(paciente);
+        }
+
+        private static void BuscarPaciente(int idPaciente)
+        {
+            var paciente = _repoPaciente.GetPaciente(idPaciente);
+            Console.WriteLine(paciente.Nombre + " " + paciente.PrimerApellido);
+
         }
     }
 }
---
+++ AgendamientoIPS.App.Persistencia/AppRepositorios/RepocitorioPaciente.cs
@@ -0,0 +1,61 @@
+using System.Collections.Generic;
+using System.Linq;
+using AgendamientoIPS.App.Dominio;
+
+namespace AgendamientoIPS.App.Persistencia
+{
+    public class RepositorioPaciente : IRepositorioPaciente
+    {
+        private readonly AppContext _appContext;
+
+        public RepositorioPaciente(AppContext appContext)
+        {
+            _appContext = appContext;
+        }
+
+        Paciente IRepositorioPaciente.AddPaciente(Paciente paciente)
+        {
+            var pacienteAdicionado = _appContext.Pacientes.Add(paciente);
+            _appContext.SaveChanges();
+            return pacienteAdicionado.Entity;
+        }
+
+        void IRepositorioPaciente.DeletePaciente(int idPaciente)
+        {
+            var pacienteEncontrado = _appContext.Pacientes.FirstOrDefault(p => p.Id == idPaciente);
+            if (pacienteEncontrado == null)
+                return;
+            _appContext.Pacientes.Remove(pacienteEncontrado);
+            _appContext.SaveChanges();
+        }
+
+        IEnumerable<Paciente> IRepositorioPaciente.GetAllPacientes()
+        {
+            return _appContext.Pacientes;
+
+        }
+
+        Paciente IRepositorioPaciente.GetPaciente(int idPaciente)
+        {
+            return _appContext.Pacientes.FirstOrDefault(p => p.Id == idPaciente);
+        }
+
+        Paciente IRepositorioPaciente.UpdatePaciente(Paciente paciente)
+        {
+            var pacienteEncontrado = _appContext.Pacientes.FirstOrDefault(p => p.Id == paciente.Id);
+            if (pacienteEncontrado != null)
+            {
+                pacienteEncontrado.Nombre = paciente.Nombre;
+                pacienteEncontrado.PrimerApellido = paciente.PrimerApellido;
+                pacienteEncontrado.SegundoApellido = paciente.SegundoApellido;
+                pacienteEncontrado.Direccion = paciente.Direccion;
+                pacienteEncontrado.Telefono = paciente.Telefono;
+                pacienteEncontrado.Correo = paciente.Correo;
+                pacienteEncontrado.EPS = paciente.EPS;
+
+                _appContext.SaveChanges();
+            }
+            return pacienteEncontrado;
+        }
+    }
+}
