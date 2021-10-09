bc2f50dc30df72b88c94a1c6780365b14a3f8af3
NeylaVillamizar <neylavillamizar@yahoo.es>
2021-10-09 18:09:01 -0500

    Avance Creates

---
+++ AgendamientoIPS.App.Frontend/Pages/Pacientes/Create.cshtml.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using AgendamientoIPS.App.Persistencia;
+using AgendamientoIPS.App.Dominio;
+
+namespace AgendamientoIPS.App.Frontend.Pages.Pacientes
+{
+    public class CreateModel : PageModel
+    {
+        private readonly IRepositorioPaciente _repoPaciente;
+        public Paciente paciente {get;set;} //propiedad tipo paciente
+        public CreateModel(IRepositorioPaciente repoPaciente) //constructor
+        {
+            _repoPaciente = repoPaciente;
+        }
+        public void OnGet()
+        {
+            paciente = new Paciente();
+        }
+        public IActionResult OnPost(Paciente paciente)
+        {
+            _repoPaciente.AddPaciente(paciente);
+            return RedirectToPage("Index");
+        }
+    }
+}
